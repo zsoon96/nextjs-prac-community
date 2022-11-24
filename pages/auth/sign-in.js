0d10492f42fdee444d8a8271e9b98331f55f45bc
zsoon96 <pres0616@gmail.com>
2022-11-25 07:40:16 +0900

    Community 예제 - 로그인 페이지 UI 및 form 라이브러리 적용(formik)

--- pages/auth/sign-in.js
+++ pages/auth/sign-in.js
@@ -1,7 +1,58 @@
+import {Formik} from 'formik';
+
 export default function SignIn() {
     return (
-        <div>
-            로그인
+        <div className='container'>
+            <h1>로그인</h1>
+            <Formik
+                // 초기값 설정
+                initialValues={{
+                    email: '',
+                    password: ''
+                }}
+                // 유효성 체크인
+                validate={values => {
+
+                }}
+                // 데이터 전송
+                onSubmit={values => {
+
+                }}
+            >
+                {
+                    ({
+                         values, errors, touched, handleChange, handleBlur, isSubmitting, handleSubmit
+                     }) => (
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="emailInput"
+                                       placeholder="name@example.com"
+                                       name='email'
+                                       value={ values.email }
+                                       onChange={ handleChange }
+                                       onBlur={ handleBlur }
+                                />
+                                <p className='text-danger mt-2'>{ errors.email && touched.email && errors.email }</p>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="passwordInput"
+                                       placeholder="Password"
+                                       name='password'
+                                       value={ values.password }
+                                       onChange={ handleChange }
+                                       onBlur={ handleBlur }
+                                />
+                                <p className='text-danger mt-2'>{ errors.password && touched.password && errors.password }</p>
+                            </div>
+                            <div>
+                                <button type='submit' className='btn btn-primary'>회원가입</button>
+                            </div>
+                        </form>
+                    )
+                }
+            </Formik>
         </div>
     )
 }
