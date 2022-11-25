a33764156234d7bb6118ef254b19db78048be206
zsoon96 <pres0616@gmail.com>
2022-11-25 21:10:41 +0900

    Community 예제 - 로그인 post 요청(axios)

--- pages/auth/sign-in.js
+++ pages/auth/sign-in.js
@@ -1,4 +1,5 @@
 import {Formik} from 'formik';
+import axios from "axios";
 
 // 이메일 주소 정규식 검증 코드
 const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -7,6 +8,7 @@ export default function SignIn() {
     return (
         <div className='container'>
             <h1>로그인</h1>
+            {/* 폼을 관리하는 라이브러리 > 반복적인 작업을 최소화 */}
             <Formik
                 // 초기값 설정
                 initialValues={{
@@ -29,14 +31,16 @@ export default function SignIn() {
                 }}
                 // 데이터 전송
                 onSubmit={values => {
-
+                    axios.post('http://localhost:3333/auth/sign-in', values)
+                        .then(response => console.log(response.data))
+                        .catch(error => console.warn(error))
                 }}
             >
                 {
                     ({
                          values, errors, touched, handleChange, handleBlur, isSubmitting, handleSubmit
                      }) => (
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
                                 <input type="email" className="form-control" id="emailInput"
@@ -46,6 +50,7 @@ export default function SignIn() {
                                        onChange={ handleChange }
                                        onBlur={ handleBlur }
                                 />
+                                {/* errors.password가 있고, touched.password가 있으면, errors.password를 출력*/}
                                 <p className='text-danger mt-2'>{ errors.email && touched.email && errors.email }</p>
                             </div>
                             <div className="mb-3">
@@ -60,7 +65,7 @@ export default function SignIn() {
                                 <p className='text-danger mt-2'>{ errors.password && touched.password && errors.password }</p>
                             </div>
                             <div>
-                                <button type='submit' className='btn btn-primary'>회원가입</button>
+                                <button type='submit' className='btn btn-primary'>로그인</button>
                             </div>
                         </form>
                     )
