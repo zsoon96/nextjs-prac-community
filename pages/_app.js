4ba7cea4b642d5e52296695de63411a3d3bd4605
zsoon96 <pres0616@gmail.com>
2022-11-27 00:25:14 +0900

    Community 예제 - 쿠키를 활용한 토큰 유지(universal-cookie)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import Head from "next/head";
+import Cookies from 'universal-cookie'
+import axios from "axios";
 
 function MyApp({ Component, pageProps }) {
+    const cookie = new Cookies()
+    const token = cookie.get('token')
+    if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`
+    }
   return(
       <>
         <Head>
--- pages/auth/sign-in.js
+++ pages/auth/sign-in.js
@@ -1,6 +1,8 @@
 import {Formik} from 'formik';
 import axios from "axios";
 import {useRouter} from "next/router";
+// 브라우저, 서버 모두 가능
+import Cookies from 'universal-cookie';
 
 // 이메일 주소 정규식 검증 코드
 const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -38,9 +40,13 @@ export default function SignIn() {
                     axios.post(process.env.API_HOST + '/auth/sign-in', values)
                         .then(response => {
                             console.log(response.data)
+                            const cookies = new Cookies()
                             const token = response.data.token.token
                             // token 헤더 전역 설정 > 페이지 이동하는 동안에는 토큰 유지가 되지만 새로고침 시에는 401
                             axios.defaults.headers.common.Authorization = `Bearer ${token}`
+                            // 쿠키를 사용하여 로그인(토큰) 유지
+                            // cookies.set('저장명', 데이터, 쿠키 적용 범위)
+                            cookies.set( 'token', token, { path: '/'})
                             router.push('/me')
                         })
                         .catch(error => {
