c79cc443ad59ef1bcf3657593783480b8da0f860
zsoon96 <pres0616@gmail.com>
2022-11-28 07:16:08 +0900

    Community 예제 - 토큰 유무 여부에 따른 렌더링(csr)

--- pages/me.js
+++ pages/me.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 import {useEffect} from "react";
+import Cookies from 'universal-cookie';
 
 export default function Me() {
+    const cookie = new Cookies();
+    const token = cookie.get('token');
+
     useEffect (()=> {
         axios.get(`${process.env.API_HOST}/me`)
             .then(response => console.log(response))
             .catch(error => console.warn(error))
     },[])
 
-    return <div className ='container'>
+    if ( !token ) {
+        return <>로그인이 필요합니다.</>
+    }
 
+    return <div className ='container'>
+        사용자 정보 표시
     </div>
 }
