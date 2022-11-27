f113f8050735df032a1b7c2097458d235b402310
zsoon96 <pres0616@gmail.com>
2022-11-28 07:22:01 +0900

    Community 예제 - 토큰 유무 여부에 따른 페이지 이동 처리(ssr)

--- pages/me.js
+++ pages/me.js
@@ -3,20 +3,32 @@ import {useEffect} from "react";
 import Cookies from 'universal-cookie';
 
 export default function Me() {
-    const cookie = new Cookies();
-    const token = cookie.get('token');
-
     useEffect (()=> {
         axios.get(`${process.env.API_HOST}/me`)
             .then(response => console.log(response))
             .catch(error => console.warn(error))
     },[])
 
-    if ( !token ) {
-        return <>로그인이 필요합니다.</>
-    }
-
     return <div className ='container'>
         사용자 정보 표시
     </div>
+}
+
+export const  getServerSideProps = async ( {req} ) => {
+    const cookie = new Cookies(req.headers.cookie)
+    const token = cookie.get('token')
+    if ( token ) {
+        return {
+            // props가 없을 경우라도 {} 빈값으로 넘겨야지 렌더링이 가능
+            props: {}
+        }
+    } else  {
+        return {
+            // redirect 통해 토큰이 없을 경우 로그인 페이지로 이동
+            redirect: {
+                destination: '/auth/sign-in',
+                permanent: false
+            }
+        }
+    }
 }
