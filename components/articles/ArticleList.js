c7c3d53d550304cd7096be2a79fd94bd503a4a0c
zsoon96 <pres0616@gmail.com>
2022-12-02 07:33:42 +0900

    Community 예제 - 게시글 목록 조회 기능(useSWR)

--- components/articles/ArticleList.js
+++ components/articles/ArticleList.js
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import useFetch from "../../hooks/useFetch";
 
 export default function ArticleList({title, category}) {
+    const { data, error } = useFetch(`${process.env.API_HOST}/articles?category=${category}`)
+    if ( error ) {
+        return<>데이터를 불러올 수 없습니다.</>
+    }
     return (
         <div className='container'>
             <h1>{title}</h1>
-            <>목록</>
+
+            <ul className='list-unstyled'>
+                {data?.data.map(article => (
+                    <li key={article.id.toString()} className='flex flex-row'>
+                        <Link href={`articles/${category}/${article.id}`}>
+                            <span className='mr-2'>{ article.id }</span>
+                            <a>{article.subject}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
 
             <div className='flex justify-end'>
                 <Link href={`/articles/${category}/create`}>
