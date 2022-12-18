c618a4c956ffaa4a81aba215c358cdffb960781e
zsoon96 <pres0616@gmail.com>
2022-12-18 15:43:33 +0900

    Community 예제 - 게시글 목록 조회 SSR/SWR 동일하게 적용

--- pages/articles/ask/index.js
+++ pages/articles/ask/index.js
@@ -1,10 +1,25 @@
 import Layout from "../../../components/Layout";
 import ArticleList from "../../../components/articles/ArticleList";
+import {fetcher} from "../../../hooks/useFetch";
+import {SWRConfig} from 'swr'
 
-export default function AskArticles() {
-    return (
+export default function AskArticles({fallback}) {
+    return <SWRConfig value={{fallback}}>
         <Layout>
             <ArticleList title='질문 게시판' category='ask'/>
         </Layout>
-    )
+    </SWRConfig>
+
+}
+
+export const getServerSideProps = async () => {
+    const url = `${process.env.API_HOST}/articles?category=ask`
+    const data = await fetcher(url)
+    return {
+        props: {
+            fallback: {
+                [url]: data
+            }
+        }
+    }
 }
