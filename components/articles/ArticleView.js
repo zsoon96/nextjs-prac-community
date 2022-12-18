7dedd04a39299fff1bba0bd8d897eb3143036435
zsoon96 <pres0616@gmail.com>
2022-12-18 15:27:52 +0900

    Community 예제 - SSR에서 SWR 적용(SWRConfig)

--- components/articles/ArticleView.js
+++ components/articles/ArticleView.js
@@ -1,14 +1,14 @@
 import useFetch from "../../hooks/useFetch";
 
-export default function ArticleView({id, article}) {
+export default function ArticleView({id}) {
     // CSR로 데이터 페칭 - 검색엔진 반영 x
-    // const { data, error } = useFetch(`${process.env.API_HOST}/articles/${id}`)
-    // if (error) {
-    //     return <> 데이터 로드에 실패했습니다.</>
-    // }
+    const { data, error } = useFetch(`${process.env.API_HOST}/articles/${id}`)
+    if (error) {
+        return <> 데이터 로드에 실패했습니다.</>
+    }
     return <div className="container">
-        <h1>{article?.subject}</h1>
+        <h1>{data?.subject}</h1>
         <hr />
-        <p>{article?.content}</p>
+        <p>{data?.content}</p>
     </div>
 }
--- pages/articles/general/[id].js
+++ pages/articles/general/[id].js
@@ -1,21 +1,28 @@
 import ArticleView from "../../../components/articles/ArticleView";
 import Layout from "../../../components/Layout";
 import {fetcher} from '../../../hooks/useFetch'
+import {SWRConfig} from 'swr'
 
-export default function ViewPage({id, article}) {
-    return <Layout>
-        <ArticleView id={id} article={article} />
-    </Layout>
+export default function ViewPage({id, fallback}) {
+    return <SWRConfig value={{fallback}}>
+        <Layout>
+            <ArticleView id={id}/>
+        </Layout>
+    </SWRConfig>
 }
 
 export const getServerSideProps = async ({params}) => {
     const id = params.id
+    const url = `${process.env.API_HOST}/articles/${id}`
     // SSR로 데이터 페칭 > 검색엔진 반영 o
-    const article = await fetcher(`${process.env.API_HOST}/articles/${id}`)
+    const article = await fetcher(url)
 
     return {
         props: {
-            id, article
+            id,
+            fallback: {
+                [url]: article
+            }
         }
     }
 }
