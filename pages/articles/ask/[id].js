6d7183a9c5864b36ce40c1422f2acd52ad31ce65
zsoon96 <pres0616@gmail.com>
2022-12-19 07:20:01 +0900

    Community 예제 - 검색엔진 또는 봇의 요청일 경우에만 SWR 적용(SSR)

--- pages/articles/ask/[id].js
+++ pages/articles/ask/[id].js
@@ -2,6 +2,7 @@ import ArticleView from "../../../components/articles/ArticleView";
 import Layout from "../../../components/Layout";
 import {fetcher} from "../../../hooks/useFetch";
 import {SWRConfig} from 'swr';
+import isbot from "isbot";
 
 export default function ViewPage({id, fallback}) {
     return <SWRConfig value={{fallback}}>
@@ -11,10 +12,11 @@ export default function ViewPage({id, fallback}) {
     </SWRConfig>
 }
 
-export const getServerSideProps = async ({params}) => {
+export const getServerSideProps = async ({req, params}) => {
     const id = params.id
     const url = `${process.env.API_HOST}/articles/${id}`
-    const article = await fetcher(url)
+    // 검색엔진이나 봇의 요청일 때만 swr 적용
+    const article = isbot(req.headers['user-agent']) ? await fetcher(url) : null;
     return {
         props: {
             id,
