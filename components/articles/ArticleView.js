67d6c60580a820931083c6d999d328cd72e0bcfd
zsoon96 <pres0616@gmail.com>
2022-12-18 14:56:48 +0900

    Community 예제 - SSR를 활용하여 게시글 상세정보 검색엔진 반영

--- components/articles/ArticleView.js
+++ components/articles/ArticleView.js
@@ -1,13 +1,14 @@
 import useFetch from "../../hooks/useFetch";
 
-export default function ArticleView({id}) {
-    const { data, error } = useFetch(`${process.env.API_HOST}/articles/${id}`)
-    if (error) {
-        return <> 데이터 로드에 실패했습니다.</>
-    }
+export default function ArticleView({id, article}) {
+    // CSR로 데이터 페칭 - 검색엔진 반영 x
+    // const { data, error } = useFetch(`${process.env.API_HOST}/articles/${id}`)
+    // if (error) {
+    //     return <> 데이터 로드에 실패했습니다.</>
+    // }
     return <div className="container">
-        <h1>{data?.subject}</h1>
+        <h1>{article?.subject}</h1>
         <hr />
-        <p>{data?.content}</p>
+        <p>{article?.content}</p>
     </div>
 }
--- pages/articles/general/[id].js
+++ pages/articles/general/[id].js
@@ -1,16 +1,21 @@
 import ArticleView from "../../../components/articles/ArticleView";
 import Layout from "../../../components/Layout";
+import {fetcher} from '../../../hooks/useFetch'
 
-export default function ViewPage({id}) {
+export default function ViewPage({id, article}) {
     return <Layout>
-        <ArticleView id={id} />
+        <ArticleView id={id} article={article} />
     </Layout>
 }
 
-export const getServerSideProps = ({params}) => {
+export const getServerSideProps = async ({params}) => {
+    const id = params.id
+    // SSR로 데이터 페칭 > 검색엔진 반영 o
+    const article = await fetcher(`${process.env.API_HOST}/articles/${id}`)
+
     return {
         props: {
-            id: params.id
+            id, article
         }
     }
 }
