b24853ae5e816359124cf60eb80687fc6ddda650
zsoon96 <pres0616@gmail.com>
2022-12-19 21:22:30 +0900

    Community 예제 - 게시글 목록 페이지네이션 적용

--- pages/articles/ask/index.js
+++ pages/articles/ask/index.js
@@ -3,20 +3,22 @@ import ArticleList from "../../../components/articles/ArticleList";
 import {fetcher} from "../../../hooks/useFetch";
 import {SWRConfig} from 'swr'
 
-export default function AskArticles({fallback}) {
+export default function AskArticles({fallback, page}) {
     return <SWRConfig value={{fallback}}>
         <Layout>
-            <ArticleList title='질문 게시판' category='ask'/>
+            <ArticleList title='질문 게시판' category='ask' page={page}/>
         </Layout>
     </SWRConfig>
 
 }
 
-export const getServerSideProps = async () => {
-    const url = `${process.env.API_HOST}/articles?category=ask`
+export const getServerSideProps = async ({query}) => {
+    const page = query.page || 1;
+    const url = `${process.env.API_HOST}/articles?category=ask&page=${page}`
     const data = await fetcher(url)
     return {
         props: {
+            page,
             fallback: {
                 [url]: data
             }
